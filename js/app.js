77a7abf311db0804234b82c285fa48b8a5fe2ea6
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-21 00:54:17 -0300

    Aula 3-5 -- Primeira tela do projeto Pokedex

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const app = new Vue({
     el: '#app',
     data: {
-        myHTML: '<input type="text" />'
+        pokemonList: [
+            {name: 'Bulbasaur', number: '001'},
+            {name: 'Ivysaur', number: '002'},
+            {name: 'Venusaur', number: '003'},
+            {name: 'Charmander', number: '004'},
+            {name: 'Charmelion', number: '005'},
+            {name: 'Charizard', number: '006'},
+            {name: 'Squirtle', number: '007'},
+            {name: 'Wartortle', number: '008'},
+            {name: 'Blastoise', number: '009'}
+        ]
     }
 })
