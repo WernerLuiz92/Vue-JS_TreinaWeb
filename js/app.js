d39de3d72f29f8fa84465234e354ae51ef8ed926
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-21 00:25:25 -0300

    Aula 3-4 -- Utilizando e manipulando HTML através do V-HTML

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 const app = new Vue({
     el: '#app',
     data: {
-        list: [
-            {text: 'item 1'},
-            {text: 'item 2'},
-            {text: 'item 2'},
-            {text: 'item 2'},
-            {text: 'item 2'},
-            {text: 'item 3'}            
-        ]
+        myHTML: '<input type="text" />'
     }
 })
